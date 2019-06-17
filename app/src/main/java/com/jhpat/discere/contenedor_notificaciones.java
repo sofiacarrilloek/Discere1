d5fe8fcd12793385e0b874e4547a2456c73085cb
YSa1r <sair1.cante@gmail.com>
2019-06-16 21:12:42 -0500

    vista de notificaciones

--- app/src/main/java/com/jhpat/discere/contenedor_notificaciones.java
+++ app/src/main/java/com/jhpat/discere/contenedor_notificaciones.java
@@ -35,7 +35,7 @@ public class contenedor_notificaciones extends AppCompatActivity {
         listDatos = new ArrayList<String>();
 
 
-        datoNoti("sofia.carrillo@cbtis72.edu.mx");
+        datoNoti("yaret.cante@cbtis72.edu.mx");
 
     }
 
