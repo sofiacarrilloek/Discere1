6d249a935a87d2cc4cd4d29666f68b041299e4cf
Sofia Abigail Carrillo Ek <44104940+sofiacarrilloek@users.noreply.github.com>
2019-08-18 22:40:20 -0500

    Pie

--- app/src/main/java/com/jhpat/discere/Tabla/Taudio_defect.java
+++ app/src/main/java/com/jhpat/discere/Tabla/Taudio_defect.java
@@ -4,12 +4,18 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.preference.PreferenceActivity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.razerdp.widget.animatedpieview.AnimatedPieView;
+import com.razerdp.widget.animatedpieview.AnimatedPieViewConfig;
+import com.razerdp.widget.animatedpieview.callback.OnPieSelectListener;
+import com.razerdp.widget.animatedpieview.data.IPieInfo;
+import com.razerdp.widget.animatedpieview.data.SimplePieInfo;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -30,8 +36,21 @@ public class Taudio_defect extends AppCompatActivity {
 
     private String id,c,n,ape;
     PieChart pieChart;
-    int[] colorClassArray = new int[]{Color.LTGRAY,Color.BLUE,Color.CYAN};
-    int[] sale = new int[]{29,23,22};
+
+    int[] colorClassArray = new int[]{Color.MAGENTA,Color.BLUE,Color.CYAN};
+    int[] sale = new int[]{29, 20};
+
+    //Pie
+    int t_critical=0, t_important=0, t_relevant=0, t_pertinent=0, t_desirable=0;
+    double p_c=0, p_i=0, p_r=0, p_p=0, p_d=0, t_p;
+
+    String color_critical="#fe6383", color_important="#63ff84", color_relevant="#7fff63",
+        color_pertinent="#8663fc", color_desirable="#6387ff";
+
+    String CRITICAL="Critical", IMPORTANT="Important", RELEVANT="Relevant", PERTINENT="Pertinent", DESIRABLE="Desirable";
+
+
+    //Pie
 
     JSONObject jsonObject;
     public static String NAME1, LAST_NAME1, GENDER1, ID1, EMAIL1, TEL1, PASSWORD1;//CLASE
@@ -47,45 +66,26 @@ public class Taudio_defect extends AppCompatActivity {
         setContentView(R.layout.tabla_audio_defect);
 
         DX1 = (TextView) findViewById(R.id.Duration1);
+        DX1.setMovementMethod(new ScrollingMovementMethod());
+
         DPX1 = (TextView) findViewById(R.id.DP1);
+        DPX1.setMovementMethod(new ScrollingMovementMethod());
+
         DTX1 = (TextView) findViewById(R.id.DT1);
+        DTX1.setMovementMethod(new ScrollingMovementMethod());
+
         DDX1 = (TextView) findViewById(R.id.DD1);
-
-
-
-        pieChart = findViewById(R.id.pieChart);
-        PieDataSet pieDataSet = new PieDataSet(Entries(),"");
-        pieDataSet.setColors(colorClassArray);
-        PieData pieData = new PieData(pieDataSet);
-        pieChart.setData(pieData);
-        pieChart.setHoleRadius(10);
-        pieChart.invalidate();
+        DDX1.setMovementMethod(new ScrollingMovementMethod());
 
        cargarPreferencias();
         //
 
-    }
-    private ArrayList<PieEntry> dataValuesl(){
-        ArrayList<PieEntry> dataVals = new ArrayList<>();
-        dataVals.add(new PieEntry(15,"Sun"));
-        dataVals.add(new PieEntry(34,"Mon"));
-        dataVals.add(new PieEntry(56,"Tue"));
-        return dataVals;
-    }
-    private ArrayList<PieEntry> Entries() {
-        ArrayList<PieEntry> entries = new ArrayList<>();
-        for (int i = 0; i < sale.length; i++)
-            entries.add(new PieEntry(sale[i]));
-        return entries;
+
 
     }
-    /*public void cargarp2(){
-        SharedPreferences preferencia =getSharedPreferences("Credenciales", Context.MODE_PRIVATE);
-        id=preferencia.getString("ID2", "NO EXISTE");
-        datosc("http://puntosingular.mx/cas/tabla/elchido?user="+id+"");
 
-    }*/
-    public void datosc(String ID_AUDIO_ANALYST) {
+
+    public void cargarAudioDefect(String ID_AUDIO_ANALYST) {
 
         AsyncHttpClient conexion = new AsyncHttpClient();
         final String url = "http://puntosingular.mx/cas/audio_defect.php"; //la url del web service
@@ -128,6 +128,36 @@ public class Taudio_defect extends AppCompatActivity {
                             type2=type2+defect_priority[i]+"\n"+"\n";
                             type3=type3+defect_type[i]+"\n"+"\n";
                             type4=type4+defect_description[i]+"\n"+"\n";
+
+
+                            //PIE Acumulador
+
+                            if (defect_priority[i].equalsIgnoreCase("desirable"))
+                            {
+                                t_desirable++;
+                            }
+                            if (defect_priority[i].equalsIgnoreCase("important"))
+                            {
+                                t_important++;
+                            }
+
+                            if (defect_priority[i].equalsIgnoreCase("critical"))
+                            {
+                                t_critical++;
+
+                            }
+                            if (defect_priority[i].equalsIgnoreCase("relevant"))
+                            {
+                                t_relevant++;
+                            }
+                            if (defect_priority[i].equalsIgnoreCase("pertinent"))
+                            {
+                                t_pertinent++;
+                            }
+
+
+                            //PIE
+
                         }
 
 
@@ -140,9 +170,17 @@ public class Taudio_defect extends AppCompatActivity {
                         DTX1.setText(""+type3);
                         DDX1.setText(""+type4);
 
-                       /* DTX2.setText(jsonObject.getJSONArray("datos").getJSONObject(1).getString("defect_type"));
-                        DDX2.setText(jsonObject.getJSONArray("datos").getJSONObject(1).getString("defect_description"));
-*/
+                        t_p=10000/tamanio;
+
+                        //Sacar porcentaje
+                        p_c=t_p*t_critical;
+                        p_d=t_p*t_desirable;
+                        p_i=t_p*t_important;
+                        p_p=t_p*t_pertinent;
+                        p_r=t_p*t_pertinent;
+
+                        dibujaGrafica(p_c,p_d,p_i, p_p, p_r );
+
 
 
 
@@ -170,15 +208,44 @@ public class Taudio_defect extends AppCompatActivity {
     }//FIN DATOSSC
     private void cargarPreferencias()
     {
-
         SharedPreferences preferencia = getSharedPreferences("Credencialestabla", Context.MODE_PRIVATE);
         String id_Analyst = preferencia.getString("Id_A", "NO EXISTE");
+        cargarAudioDefect(id_Analyst);
+    }
 
-        datosc(id_Analyst);
+    private void dibujaGrafica(double v_Critical, double v_Desirable,double
+            v_Important, double v_Pertinent, double v_Relevant)
+    {
+
+        //10,000 el valor
+
+        AnimatedPieView animatedPieView = findViewById(R.id.pieView);
+        AnimatedPieViewConfig config = new AnimatedPieViewConfig();
+        config.addData(new SimplePieInfo(v_Critical, Color.parseColor(color_critical), CRITICAL));
+        config.addData(new SimplePieInfo(v_Desirable, Color.parseColor(color_desirable), DESIRABLE));
+        config.addData(new SimplePieInfo(v_Important, Color.parseColor(color_important), IMPORTANT));
+        config.addData(new SimplePieInfo(v_Pertinent, Color.parseColor(color_pertinent), PERTINENT));
+        config.addData(new SimplePieInfo(v_Relevant, Color.parseColor(color_relevant), RELEVANT));
+
+
+        config.duration(1000);
+        config.drawText(true);
+        config.strokeMode(false);
+        config.textSize(52);
+        config.selectListener(new OnPieSelectListener<IPieInfo>() {
+            @Override
+            public void onSelectPie(@NonNull IPieInfo pieInfo, boolean isFloatUp) {
 
 
 
 
+                Toast.makeText(Taudio_defect.this, pieInfo.getDesc() + " - " + pieInfo.getValue()/100+"% ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        config.startAngle(-180);
+
+        animatedPieView.applyConfig(config);
+        animatedPieView.start();
 
 
     }
