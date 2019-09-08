939f8bf67eaf11e4436754a73da23e388386fbdf
denniscetina <denniscetina@users.noreply.github.com>
2019-09-08 16:24:05 -0500

    Subir Audio

--- app/src/main/java/com/jhpat/discere/UploadAudio.java
+++ app/src/main/java/com/jhpat/discere/UploadAudio.java
@@ -1,4 +1,8 @@
 package com.jhpat.discere;
+import android.content.ContentUris;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.Manifest;
@@ -74,7 +78,7 @@ public class UploadAudio extends AppCompatActivity implements PermissionCallback
 
                 } else {
                     //If permission is not present request for the same.
-                    EasyPermissions.requestPermissions(UploadAudio.this, "This app needs access to your file storage so that it can read files.", READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+                    EasyPermissions.requestPermissions(UploadAudio.this, getString(R.string.read_file), READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
                 }
 
             }
@@ -86,6 +90,8 @@ public class UploadAudio extends AppCompatActivity implements PermissionCallback
                 if (file != null) {
                     UploadAsyncTask uploadAsyncTask = new UploadAsyncTask(UploadAudio.this);
                     uploadAsyncTask.execute();
+                    //UploadAsyncTask.setNotificationConfig(new UploadAsyncTask());
+
 
                 } else {
                     Toast.makeText(getApplicationContext(),
@@ -100,8 +106,12 @@ public class UploadAudio extends AppCompatActivity implements PermissionCallback
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_FILE_CODE && resultCode == Activity.RESULT_OK) {
             fileUri = data.getData();
-            previewFile(fileUri);
-
+            // previewFile(fileUri);
+            String filePath = getRealPath(UploadAudio.this, fileUri);
+            file = new File(filePath);
+            Log.d(TAG, "Filename " + file.getName());
+            fileName.setText(filePath);
+            hideFileChooser();
         }
     }
 
@@ -109,32 +119,7 @@ public class UploadAudio extends AppCompatActivity implements PermissionCallback
      * Show the file name and preview once the file is chosen
      * @param uri
      */
-    private void previewFile(Uri uri) {
-        String filePath = getRealPathFromURIPath(uri, UploadAudio.this);
-        file = new File(filePath);
-        Log.d(TAG, "Filename " + file.getName());
-        fileName.setText(file.getName());
 
-        ContentResolver cR = this.getContentResolver();
-        String mime = cR.getType(uri);
-
-        //Show preview if the uploaded file is an image.
-        if (mime != null && mime.contains("image")) {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-
-            // down sizing image as it throws OutOfMemory Exception for larger
-            // images
-            options.inSampleSize = 8;
-
-            final Bitmap bitmap = BitmapFactory.decodeFile(filePath, options);
-
-            previewImage.setImageBitmap(bitmap);
-        } else {
-            previewImage.setImageResource(R.drawable.ic_file);
-        }
-
-        hideFileChooser();
-    }
 
     /**
      * Shows an intent which has options from which user can choose the file like File manager, Gallery etc
@@ -147,30 +132,104 @@ public class UploadAudio extends AppCompatActivity implements PermissionCallback
 
     }
 
-    /**
-     * Returns the actual path of the file in the file system
-     *
-     * @param contentURI
-     * @param activity
-     * @return
-     */
-    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
-        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
-        String realPath = "";
-        if (cursor == null) {
-            realPath = contentURI.getPath();
-        } else {
-            cursor.moveToFirst();
-            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            realPath = cursor.getString(idx);
-        }
-        if (cursor != null) {
-            cursor.close();
+
+
+    public static String getRealPath(final Context context, final Uri uri) {
+
+        if (uri.getScheme().equals("file")) {
+            return uri.toString();
+
+        } else if (uri.getScheme().equals("content")) {
+            // DocumentProvider
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                if (DocumentsContract.isDocumentUri(context, uri)) {
+
+                    // ExternalStorageProvider
+                    if (isExternalStorageDocument(uri)) {
+                        final String docId = DocumentsContract.getDocumentId(uri);
+                        final String[] split = docId.split(":");
+                        final String type = split[0];
+
+                        if ("primary".equalsIgnoreCase(type)) {
+                            return Environment.getExternalStorageDirectory() + "/" + split[1];
+                        }
+
+                        // TODO handle non-primary volumes
+                    }
+                    // DownloadsProvider
+                    else if (isDownloadsDocument(uri)) {
+
+                        final String id = DocumentsContract.getDocumentId(uri);
+                        final Uri contentUri = ContentUris.withAppendedId(
+                                Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                        return getDataColumn(context, contentUri, null, null);
+                    }
+                    // MediaProvider
+                    else if (isMediaDocument(uri)) {
+                        final String docId = DocumentsContract.getDocumentId(uri);
+                        final String[] split = docId.split(":");
+                        final String type = split[0];
+
+                        Uri contentUri = null;
+                        if ("image".equals(type)) {
+                            contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                        } else if ("video".equals(type)) {
+                            contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                        } else if ("audio".equals(type)) {
+                            contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                        }
+
+                        final String selection = "_id=?";
+                        final String[] selectionArgs = new String[]{
+                                split[1]
+                        };
+
+                        return getDataColumn(context, contentUri, selection, selectionArgs);
+                    }
+                }
+            }
         }
 
-        return realPath;
+        return null;
     }
 
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -298,6 +357,5 @@ public class UploadAudio extends AppCompatActivity implements PermissionCallback
             this.progressDialog.setProgress((int) progress[0]);
         }
     }
-
 }
 
