2ad2364ccdd21c4b84a1da1518e4e38cd09649f4
Sofia Abigail Carrillo Ek <44104940+sofiacarrilloek@users.noreply.github.com>
2020-01-06 12:31:50 -0500

    CALENDARIO

--- app/src/main/java/com/jhpat/discere/MainActivity2.java
+++ app/src/main/java/com/jhpat/discere/MainActivity2.java
@@ -112,53 +112,13 @@ public class MainActivity2 extends AppCompatActivity{
 
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 String selectedGridDate = HwAdapter.day_string.get(position);
-                String fecham= selectedGridDate;
                 ((HwAdapter) parent.getAdapter()).getPositionList(selectedGridDate, MainActivity2.this);
 
             }
 
         });
     }
-    public void obtenerFecha (String Correo)
-    {
-//Conexion
-        AsyncHttpClient conexion = new AsyncHttpClient();
-        final String url ="http://34.226.77.86/discere/cas/obtenerS.php"; //la url del web service
-        final RequestParams requestParams =new RequestParams();
-        requestParams.add("fecha",Correo); //envio el parametro
 
-        conexion.post(url, requestParams, new AsyncHttpResponseHandler() {
-
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
-
-                try {
-                    jsonObjecte = new JSONObject(new String(responseBody));
-                    //Apartir de aqui, les asigno a los editText el valor que obtengo del webservice
-
-                    nombreE=jsonObjecte.getJSONArray("datos").getJSONObject(0).getString("name_fellow");
-                    fechaE=jsonObjecte.getJSONArray("datos").getJSONObject(0).getString("date");
-                    horaIE=jsonObjecte.getJSONArray("datos").getJSONObject(0).getString("start_time");
-                    horaFE=jsonObjecte.getJSONArray("datos").getJSONObject(0).getString("end_time");
-
-
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
-            }
-        });
-
-    }
     protected void setNextMonth() {
         if (cal_month.get(GregorianCalendar.MONTH) == cal_month.getActualMaximum(GregorianCalendar.MONTH)) {
             cal_month.set((cal_month.get(GregorianCalendar.YEAR) + 1), cal_month.getActualMinimum(GregorianCalendar.MONTH), 1);
